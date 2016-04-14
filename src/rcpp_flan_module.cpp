ed83760a00e6e9054a67a4e356ecf2c777a68ec9
R cqls <rdrouilh@gmail.com>
2016-04-14 22:12:41 +0200

    add alglib temporarily

--- src/rcpp_flan_module.cpp
+++ src/rcpp_flan_module.cpp
@@ -5,21 +5,18 @@
 // }
 
 RCPP_MODULE(flan_module) {
-    class_<FLAN_Sim>("FlanSim")
+  class_<FLAN_Sim>("FlanSim")
 	.constructor<List>()
 	.method("rflan",&FLAN_Sim::computeSamplesMutantsFinalsNumber,"compute sample mutants")
-    ;
-    
-    class_<FLAN_MutationModel>("FlanMutMod")
+  ;
+
+  class_<FLAN_MutationModel>("FlanMutMod")
 	.constructor<List>()
 	.method("pflan",&FLAN_MutationModel::computeCumulativeFunction,"compute cumulative function")
 	.method("dflan",&FLAN_MutationModel::computeProbability,"compute probability")
 // 	.method("unbias_mutprob_estimation",&FLAN_MutationModel::unbiasPiEstimation,"unbias mutprob estimation")
-    ;
-    
+  ;
+
   //function( "newMaintenancePolicy", &newMaintenancePolicy );
 
 }
-
-
-
